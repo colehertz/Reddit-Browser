bd9b126659f1df54f8af4485730e14baac2c51cd
colehertz <chrh02@gmail.com>
2015-06-17 16:01:30 -0700

    Initial Commit

---
+++ redditBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redditBrowser
+//
+//  Created by Cole H on 6/17/15.
+//  Copyright (c) 2015 Yep Technologies LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
